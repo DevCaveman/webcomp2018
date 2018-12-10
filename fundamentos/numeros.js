fb6e76849f1df7e4bfcd14c528c527dfcf018054
Thiago G.Martins <devthiago.py@gmail.com>
2018-12-10 19:19:36 -0200

    Adicionado mais exemplos.

--- fundamentos/numeros.js
+++ fundamentos/numeros.js
@@ -16,3 +16,10 @@ console.log('4º -> ' + num4 + ' do tipo ' + typeof num4);
 console.log('5º -> ' + num5 + ' em binário -> ' + num5.toString(2)); // num.toString(2) = binário
 console.log('6º -> ' + num6 + ' do tipo ' + typeof num6);
 console.log('6º -> ' + num6.toFixed(2) + ' com apenas 2 casas deciamis ');
+
+// Alguns cuidados
+console.log(7 / 0); //Tipo Infinity
+console.log('10.2' * 2); // Converte automaticamente para Number(repeitar syntax, ex 10,2 ñ funciona)
+console.log('10.2' + 2); // ATENCAO! resultado 10.22 em vêz de 12.2
+console.log((10.2122).toFixed(2)); // ATENCAO! Expressões literais tem que estar entre parenteses, ex (10.2122)
+//console.log(10.2122.toFixed(2)); NAO FUNCIONA!
