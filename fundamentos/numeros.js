dfa3611df94ec0c1d9df54cad2720fc0f2250628
Thiago G.Martins <devthiago.py@gmail.com>
2018-12-10 19:05:19 -0200

    Add numeros.js

---
+++ fundamentos/numeros.js
@@ -0,0 +1,18 @@
+/*
+ * Numeros em JavaScript
+ */
+const num1 = 1; // Integer
+const num2 = 2.0; // Integer O_O
+const num3 = 3.1; // Float
+const num4 = Number('4.0'); // Convert String para Number
+const num5 = 123;
+const num6 = 123.1234567; // Float
+
+console.log('Números: ');
+console.log('1º -> ' + num1 + ' do tipo ' + typeof num1);
+console.log('2º -> ' + num2 + ' do tipo ' + typeof num2);
+console.log('3º -> ' + num3 + ' do tipo ' + typeof num3);
+console.log('4º -> ' + num4 + ' do tipo ' + typeof num4);
+console.log('5º -> ' + num5 + ' em binário -> ' + num5.toString(2)); // num.toString(2) = binário
+console.log('6º -> ' + num6 + ' do tipo ' + typeof num6);
+console.log('6º -> ' + num6.toFixed(2) + ' com apenas 2 casas deciamis ');
