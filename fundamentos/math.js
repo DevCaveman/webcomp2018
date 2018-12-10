c85e8758b96708b37bbbe937a0870721df972f9e
Thiago G.Martins <devthiago.py@gmail.com>
2018-12-10 19:30:12 -0200

    Add

---
+++ fundamentos/math.js
@@ -0,0 +1,6 @@
+/* Usando a lib Math para calcular raios, PI e muito mais */
+
+const raio = 5.3;
+const area = Math.PI * Math.pow(raio, 2); // Multiplicando 3.14 pelo quadrado de raio
+
+console.log('A área de ' + raio + ' é ' + area.toFixed(2));
